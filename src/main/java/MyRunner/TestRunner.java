2756febe1b7cbf7b12af5c21bc80bded4e9903f0
Saksham Agarwal <100130105+saksham2541996@users.noreply.github.com>
2022-06-21 19:40:36 +0530

    Made changes to fix the code
    The repo was not working so I changed the dependencies and made changes to test runner

--- src/main/java/MyRunner/TestRunner.java
+++ src/main/java/MyRunner/TestRunner.java
@@ -2,6 +2,9 @@ package MyRunner;
 
 import java.net.URL;
 
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.TestNGCucumberRunner;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -10,24 +13,14 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.testng.CucumberFeatureWrapper;
-import cucumber.api.testng.TestNGCucumberRunner;
 
 @CucumberOptions(
-        features = "src/main/java/Features",
+        features = "src/main/java/Features/todo.feature",
         glue = {"stepDefinitions"},
-        tags = {"~@Ignore"},
-        format = {
-                "pretty",
-                "html:target/cucumber-reports/cucumber-pretty",
-                "json:target/cucumber-reports/CucumberTestReport.json",
-                "rerun:target/cucumber-reports/rerun.txt"
-        },plugin = "json:target/cucumber-reports/CucumberTestReport.json")
+        plugin = "json:target/cucumber-reports/CucumberTestReport.json")
 
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests {
 	
     private TestNGCucumberRunner testNGCucumberRunner;
   
@@ -63,19 +56,15 @@ public class TestRunner {
     		System.out.println(capability);
     		System.out.println(connection.getSessionId());
 }
- 
-    @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
-    public void feature(CucumberFeatureWrapper cucumberFeature) {
-        testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
-    }
- 
+
+
     @DataProvider
     public Object[][] features() {
-        return testNGCucumberRunner.provideFeatures();
+        return testNGCucumberRunner.provideScenarios();
     }
  
     @AfterClass(alwaysRun = true)
-    public void tearDownClass() throws Exception {
+    public void tearDownClass() {
         testNGCucumberRunner.finish();
     }
 }
